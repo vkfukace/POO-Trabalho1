b1790a80084b19b4806fbafbe8bec7f6f28c1be8
vini-kenzo <vkf5@hotmail.com>
2021-04-07 14:50:06 -0300

    aa

--- src/Entidades/RecursoHumano/Professor.java
+++ src/Entidades/RecursoHumano/Professor.java
@@ -2,28 +2,13 @@ package Entidades.RecursoHumano;
 
 public class Professor extends Pessoa {
 	private String regProfessor;
-<<<<<<< HEAD
-	
-=======
 
-<<<<<<< HEAD
 	public Professor(String regProfessor, String nome, String rg, int idade) {
 		super(nome, rg, idade);
 		this.regProfessor = regProfessor;
 	}
 
-=======
->>>>>>> 5e91f5156465ae85237ba57352775228281d52b5
 	public String getRegProfessor() {
 		return this.regProfessor;
 	}
-
-<<<<<<< HEAD
-=======
-	public void setRegProfessor(String regProfessor) {
-		this.regProfessor = regProfessor;
-	}
-
->>>>>>> 09dd79da104f3fbdfae731420f57d6416ac91163
->>>>>>> 5e91f5156465ae85237ba57352775228281d52b5
 }
